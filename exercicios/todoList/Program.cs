e740208811876c7c2d5713cc950850dd7b3bb88a
userpaulo123 <miolomole30@gmail.com>
2019-10-10 11:26:54 -0300

    alteraçao

---
+++ exercicios/todoList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace todolist
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            List<string> todoList= new List<string>();
+            string fileName = "todo.csv";
+            string filePath = ".//"  + fileName;
+
+            try{
+            string[] todofile = File.ReadAllLines(@filePath);
+            
+            foreach(string line in todofile){
+                todoList.Add(line);
+            }
+            } catch(IOException e){
+                Console.WriteLine("Error Acesso.");
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+            int count = 1;
+            {
+            foreach (string item in todoList);
+            Console.WriteLine($"count: {count,3}:, {item}");
+            count++;
+        
+        }
+        return 0;
+
+    }
+}
